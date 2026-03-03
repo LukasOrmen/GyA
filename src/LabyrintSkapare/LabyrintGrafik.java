8c3508c6228593f6039cabf9ea1de59f3384a441
SvampKonrad <sixten.axelsson@skola.uppsala.se>
2026-03-03 19:34:59 +0100

    LabyrintGrafik helt klar, tre olika musar löser labyrint med tre lösningar med hjälp av en knapp

--- src/LabyrintSkapare/LabyrintGrafik.java
+++ src/LabyrintSkapare/LabyrintGrafik.java
@@ -1,7 +1,6 @@
 package LabyrintSkapare;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -11,20 +10,29 @@ public class LabyrintGrafik {
     // Hela fönstret
     private JFrame frame;
 
+    // Panel med alla paneler
+    JPanel gridPanel;
+
     private LinkedList<JPanel> panels;
     private int xPanelAntal;
     private int yPanelAntal;
 
-    // Interfacet
+    // Interfacen
     Solver solver;
+    Solver solver2;
+    Solver solver3;
 
     // Path från interfacet
     LinkedList<String> path;
 
-    // Musen
+    // Mössen
     private ImageIcon mouseIcon;
-    private JLabel mouseLabel;
     private Image scaledMouse;
+    private ImageIcon mouseIcon2;
+    private Image scaledMouse2;
+    private ImageIcon mouseIcon3;
+    private Image scaledMouse3;
+    private JLabel mouseLabel;
 
     // Osten
     private ImageIcon cheeseIcon;
@@ -41,14 +49,23 @@ public class LabyrintGrafik {
     // Statistiklabeln när musen är klar
     private JLabel endingLabel;
 
+    // Omstartsknappen
+    private JButton restartButton;
+    private int restartClicks = 0;
+
     // Konstruktorn
-    public LabyrintGrafik(Labyrint lab, Solver solver) {
+    public LabyrintGrafik(Labyrint lab, Solver solver, Solver solver2, Solver solver3) {
+
         this.xPanelAntal = lab.getX();
         this.yPanelAntal = lab.getY();
         this.solver = solver;
+        this.solver2 = solver2;
+        this.solver3 = solver3;
 
         // Själva fönstret
         frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+
         // Lösningsvägen
         path = solver.getPath();
         // Listan för panelerna
@@ -59,52 +76,63 @@ public class LabyrintGrafik {
         initiateLabels();
         drawMaze(lab);
         drawSolution();
+        createRestartButton();
     }
 
     // Ritar labyrinten med hjälp av panelerna
     private void drawMaze(Labyrint lab) {
+
+        // Större panel som innehåller alla paneler - för att omstartsknappen ska kunna finnas
+        gridPanel = new JPanel();
+        gridPanel.setLayout(new GridLayout(xPanelAntal, yPanelAntal));
+        gridPanel.setBackground(Color.BLACK);
+
         Cell[][] cells = new MakeCells(lab).getCells();
-        frame.setLayout(new GridLayout(xPanelAntal, yPanelAntal));
 
         for (int y = 0; y < yPanelAntal; y++) {
             for (int x = 0; x < xPanelAntal; x++) {
+
                 Cell c = cells[y][x];
 
-                int top    = c.getTop();
-                int left   = c.getLeft();
-                int bottom = c.getBottom();
-                int right  = c.getRight();
-
                 JPanel panel = new JPanel();
-                panel.setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, Color.BLACK));
+                panel.setBorder(BorderFactory.createMatteBorder(
+                        c.getTop(),
+                        c.getLeft(),
+                        c.getBottom(),
+                        c.getRight(),
+                        Color.BLACK));
+
                 panel.setBackground(new Color(156, 236, 126));
 
                 panels.add(panel);
-                frame.add(panel);
+                gridPanel.add(panel);
             }
         }
 
-        // Ritar fönsret
-        frame.setSize(500, 500);
+        frame.add(gridPanel, BorderLayout.CENTER);
+
+        // Ritar fönstret
+        frame.setSize(500, 550);
         frame.setLocation(430, 100);
-        // frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     // Musen löser labyrinten
     public void drawSolution() {
-        // Lägger in nycklar och värden i panel-HashMapen
-        LinkedList<String> cordList = new LinkedList<>();
-        for (int i = 0; i < xPanelAntal; i++) {
-           for (int j = 0; j < yPanelAntal; j++) {
-               char letter = (char) (65 + i);
-               int number = j + 1;
-               String cord = letter + "" + number;
 
-               cordList.add(cord);
-           }
-       }
+        steg = 0;
+
+        LinkedList<String> cordList = new LinkedList<>();
+
+        for (int i = 0; i < xPanelAntal; i++) {
+            for (int j = 0; j < yPanelAntal; j++) {
+                char letter = (char) (65 + i);
+                int number = j + 1;
+                cordList.add(letter + "" + number);
+            }
+        }
+
         for (int i = 0; i < panels.size(); i++) {
             panelMap.put(cordList.get(i), panels.get(i));
         }
@@ -115,57 +143,112 @@ public class LabyrintGrafik {
         panels.getLast().repaint();
 
         // Musen flyttas en panel i taget
-        timer = new Timer(200, e -> {
+        timer = new Timer(50, e -> {
+
             // Lägger till musen på nuvarande panel
             String currentCord = path.get(steg);
-            panelMap.get(currentCord).add(mouseLabel);
 
-            // Behövs för att rita om en panel
+            panelMap.get(currentCord).add(mouseLabel);
             panelMap.get(currentCord).revalidate();
             panelMap.get(currentCord).repaint();
 
-            // Tar bort musen från panelen innan
             if (steg > 0) {
-                String recentCord = path.get(steg - 1);
-                panelMap.get(recentCord).remove(mouseLabel);
-                panelMap.get(recentCord).revalidate();
-                panelMap.get(recentCord).repaint();
+                String previousCord = path.get(steg - 1);
+                panelMap.get(previousCord).remove(mouseLabel);
+                panelMap.get(previousCord).revalidate();
+                panelMap.get(previousCord).repaint();
             }
 
-            // Ökar antal steg med 1
-            steg++;
-            if (steg == path.size()) {
-                timer.stop();
+            // Osten tas bort när musen gått klart
+            if (steg == path.size() - 1) {
 
-                // Tar bort osten när musen är framme
                 panels.getLast().remove(cheeseLabel);
                 panels.getLast().revalidate();
                 panels.getLast().repaint();
 
-                // Visar statistiklabeln:
                 endingLabel.setVisible(true);
+
+                timer.stop();
             }
-        } );
+
+            steg++;
+        });
+
         timer.start();
     }
 
+    // Knappen längst ned
+    private void createRestartButton() {
+
+        restartButton = new JButton("Nästa lösning");
+        restartButton.setFocusPainted(false);
+        restartButton.setBackground(new Color(172, 3, 3));
+        restartButton.setForeground(Color.WHITE);
+
+        restartButton.addActionListener(e -> {
+
+            restartClicks++;
+
+            if (timer != null) {
+                timer.stop();
+            }
+
+            for (JPanel panel : panels) {
+                panel.remove(mouseLabel);
+                panel.revalidate();
+                panel.repaint();
+            }
+
+            endingLabel.setVisible(false);
+
+            panels.getLast().add(cheeseLabel);
+            panels.getLast().revalidate();
+            panels.getLast().repaint();
+
+            if (restartClicks == 1) {
+                path = solver2.getPath();
+                endingLabel.setText("Antal steg: " + (path.size() - 1));
+                mouseLabel.setIcon(new ImageIcon(scaledMouse2));
+            }
+
+            if (restartClicks == 2) {
+                path = solver3.getPath();
+                endingLabel.setText("Antal steg: " + (path.size() - 1));
+                mouseLabel.setIcon(new ImageIcon(scaledMouse3));
+            }
+
+            drawSolution();
+
+            if (restartClicks == 2) {
+                restartButton.setEnabled(false);
+                restartButton.setBackground(Color.GRAY);
+            }
+        });
+
+        frame.add(restartButton, BorderLayout.SOUTH);
+    }
+
     public void initiateLabels() {
-        // Musen
-        mouseIcon = new ImageIcon("bilder/mus.png");
+
+        mouseIcon = new ImageIcon("bilder/musse.png");
         scaledMouse = mouseIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
         mouseLabel = new JLabel(new ImageIcon(scaledMouse));
 
-        // Osten
+        mouseIcon2 = new ImageIcon("bilder/jerry.png");
+        scaledMouse2 = mouseIcon2.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        mouseIcon3 = new ImageIcon("bilder/minniemouse.png");
+        scaledMouse3 = mouseIcon3.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+
         cheeseIcon = new ImageIcon("bilder/cheese.png");
         scaledCheese = cheeseIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
         cheeseLabel = new JLabel(new ImageIcon(scaledCheese));
 
-        // Statistiklabeln när musen är klar
         endingLabel = new JLabel("Antal steg: " + (path.size() - 1));
-        endingLabel.setFont(new Font("", Font.PLAIN, 60));
+        endingLabel.setFont(new Font("SansSerif", Font.BOLD, 62));
         endingLabel.setHorizontalAlignment(SwingConstants.CENTER);
         endingLabel.setVerticalAlignment(SwingConstants.CENTER);
-        endingLabel.setForeground(new Color(7, 19, 60, 255));
+        endingLabel.setForeground(new Color(172, 3, 3));
+
         frame.setGlassPane(endingLabel);
     }
 }
