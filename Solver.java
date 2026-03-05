6ac0ea54d0256cd2a80bea3d6f5b84ceb3568b8e
sirwanmalek-coder <sirwanmalek@gmail.com>
2026-03-05 13:18:28 +0100

    solve metod

--- Solver.java
+++ Solver.java
@@ -3,6 +3,8 @@ package LabyrintSkapare;
 import java.util.LinkedList;
 
 public interface Solver {
+    void solve(int startX, int startY, int goalX, int goalY);
+
     LinkedList<String> getPath();
 
     LinkedList<String> getIntersections();
